bd91765048b777b4d416bab9e11b473449c0d93a
Zongchun Yu <zongchun.yu@nxp.com>
2020-12-15 14:22:09 +0800

    src/build.sh: Add build support for pytorch v1.7.1 and torchvision v0.8.2
    Signed-off-by: Zongchun Yu <zongchun.yu@nxp.com>

--- src/build.sh
+++ src/build.sh
@@ -10,13 +10,14 @@ if [ ! -d pytorch ]; then
   git clone --recursive https://github.com/pytorch/pytorch
 fi
 cd pytorch
-git checkout v1.6.0 -b v1.6.0
-git reset --hard v1.6.0
+git checkout v1.7.1 -b v1.7.1
+git reset --hard v1.7.1
 git submodule sync
 git submodule update --init --recursive
 
-# patch the code with fix on aarch64
-git am $CWD/0001-QNNPACK-q8gemm-8x8-dq-aarch64-neon.S-fix-mov-operand.patch
+cd third_party/sleef
+git reset --hard 3.5.1
+cd -
 
 # build pytorch wheel file
 export MAX_JOBS=$(nproc)
@@ -24,7 +25,7 @@ export USE_CUDA=0
 export USE_NNPACK=0
 export USE_QNNPACK=0
 export CMAKE_PREFIX_PATH=/usr/bin
-export PYTORCH_BUILD_VERSION=1.6.0
+export PYTORCH_BUILD_VERSION=1.7.1
 export PYTORCH_BUILD_NUMBER=1
 
 python3 setup.py bdist_wheel
@@ -39,17 +40,17 @@ if [ ! -d vision ]; then
   git clone https://github.com/pytorch/vision.git
 fi
 cd vision
-git checkout v0.7.0 -b v0.7.0
-git reset --hard v0.7.0
+git checkout v0.8.2 -b v0.8.2
+git reset --hard v0.8.2
 
 # pytorch is build dependcy for torchvision, check and install it.
-torch_pkg="$(python3 -m pip list --format=freeze | grep "torch==1.6.0")"
+torch_pkg="$(python3 -m pip list --format=freeze | grep "torch==1.7.1")"
 if [ -z "$torch_pkg" ]; then
   torch_wheel=$(ls $CWD/pytorch/dist/torch-*.whl)
   yes | python3 -m pip install $torch_wheel
 fi
 
-export BUILD_VERSION=0.7.0
+export BUILD_VERSION=0.8.2
 python3 setup.py bdist_wheel
 
 # check the wheel file in dist folder
