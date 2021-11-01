1ada8b28230a84a30f14c57c75af8d733f087927
Xuelin Shi <xuelin.shi@nxp.com>
2021-11-01 14:08:32 +0800

    update build scrip to support torch-v1.9.1 and torchvision-v0.10.0
    Signed-off-by: Xuelin Shi <xuelin.shi@nxp.com>

--- src/build.sh
+++ src/build.sh
@@ -7,11 +7,11 @@ CWD=$(dirname $(readlink -f "$0"))
 
 # check out pytorch code
 if [ ! -d pytorch ]; then
-  git clone --recursive https://github.com/pytorch/pytorch
+  git clone --recursive git://github.com/pytorch/pytorch
 fi
 cd pytorch
-git checkout v1.7.1 -b v1.7.1
-git reset --hard v1.7.1
+git checkout v1.9.1 -b v1.9.1
+git reset --hard v1.9.1
 git submodule sync
 git submodule update --init --recursive
 
@@ -20,12 +20,12 @@ git reset --hard 3.5.1
 cd -
 
 # build pytorch wheel file
-export MAX_JOBS=$(nproc)
+export MAX_JOBS=2
 export USE_CUDA=0
 export USE_NNPACK=0
 export USE_QNNPACK=0
 export CMAKE_PREFIX_PATH=/usr/bin
-export PYTORCH_BUILD_VERSION=1.7.1
+export PYTORCH_BUILD_VERSION=1.9.1
 export PYTORCH_BUILD_NUMBER=1
 
 python3 setup.py bdist_wheel
@@ -37,20 +37,20 @@ python3 setup.py bdist_wheel
 # build the wheel file for torchvision
 cd $CWD
 if [ ! -d vision ]; then
-  git clone https://github.com/pytorch/vision.git
+  git clone git://github.com/pytorch/vision.git
 fi
 cd vision
-git checkout v0.8.2 -b v0.8.2
-git reset --hard v0.8.2
+git checkout v0.10.0 -b v0.10.0
+git reset --hard v0.10.0
 
 # pytorch is build dependcy for torchvision, check and install it.
-torch_pkg="$(python3 -m pip list --format=freeze | grep "torch==1.7.1")"
+torch_pkg="$(python3 -m pip list --format=freeze | grep "torch==1.9.1")"
 if [ -z "$torch_pkg" ]; then
   torch_wheel=$(ls $CWD/pytorch/dist/torch-*.whl)
   yes | python3 -m pip install $torch_wheel
 fi
 
-export BUILD_VERSION=0.8.2
+export BUILD_VERSION=0.10.0
 python3 setup.py bdist_wheel
 
 # check the wheel file in dist folder
