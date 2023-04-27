eab552fc225d175d22d95cfbfd3fe7c0b55681ab
Xuelin Shi <xuelin.shi@nxp.com>
2023-04-27 14:29:36 +0800

    upgrade to torch 2.0.0 and aligned to python 3.11
    Signed-off-by: Xuelin Shi <xuelin.shi@nxp.com>

--- src/build.sh
+++ src/build.sh
@@ -10,8 +10,8 @@ if [ ! -d pytorch ]; then
   git clone --recursive git://github.com/pytorch/pytorch
 fi
 cd pytorch
-git checkout v1.9.1 -b v1.9.1
-git reset --hard v1.9.1
+git checkout v2.0.0 -b v2.0.0
+git reset --hard v2.0.0
 git submodule sync
 git submodule update --init --recursive
 
@@ -24,8 +24,11 @@ export MAX_JOBS=2
 export USE_CUDA=0
 export USE_NNPACK=0
 export USE_QNNPACK=0
+export USE_DISTRIBUTED=0
+export USE_MLDNN=0
+export USE_FBGEMM=0
 export CMAKE_PREFIX_PATH=/usr/bin
-export PYTORCH_BUILD_VERSION=1.9.1
+export PYTORCH_BUILD_VERSION=2.0.0
 export PYTORCH_BUILD_NUMBER=1
 
 python3 setup.py bdist_wheel
@@ -40,17 +43,18 @@ if [ ! -d vision ]; then
   git clone git://github.com/pytorch/vision.git
 fi
 cd vision
-git checkout v0.10.0 -b v0.10.0
-git reset --hard v0.10.0
+git checkout v0.15.1 -b v0.15.1
+git reset --hard v0.15.1
 
 # pytorch is build dependcy for torchvision, check and install it.
-torch_pkg="$(python3 -m pip list --format=freeze | grep "torch==1.9.1")"
+torch_pkg="$(python3 -m pip list --format=freeze | grep "torch==2.0.0")"
 if [ -z "$torch_pkg" ]; then
   torch_wheel=$(ls $CWD/pytorch/dist/torch-*.whl)
   yes | python3 -m pip install $torch_wheel
 fi
 
-export BUILD_VERSION=0.10.0
+export BUILD_VERSION=0.15.1
+export TORCHVISION_USE_FFMPEG=0
 python3 setup.py bdist_wheel
 
 # check the wheel file in dist folder
